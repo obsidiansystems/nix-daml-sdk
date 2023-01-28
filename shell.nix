243c94b39837c9cb25a8191379b098962ed36622
Ali Abrar <ali.abrar@obsidian.systems>
2023-01-27 21:13:38 -0400

    Nixify daml studio

--- shell.nix
+++ shell.nix
@@ -1,8 +1,24 @@
+{ vimMode ? false }:
 let
   rp = import ./reflex-platform {};
   pkgs = rp.nixpkgs;
+  vscodeWithExtensions = pkgs.vscode-with-extensions.override {
+    vscodeExtensions = with pkgs.vscode-extensions; [
+      haskell.haskell
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      { name = "daml";
+        publisher = "DigitalAssetHoldingsLLC";
+        version = "2.5.1";
+        sha256 = "sha256-joe3j38AymdVfqSdVOc45LXpfCNnCWJxDeSRT8IPHsk=";
+      }
+    ] ++ pkgs.lib.optional vimMode vscodevim.vim ;
+  };
 in
   pkgs.mkShell {
     name = "daml-sdk";
-    packages = [ (import ./. { inherit (pkgs) lib stdenv jdk; }) ];
+    packages = [
+      (import ./. { inherit (pkgs) lib stdenv jdk; })
+      vscodeWithExtensions
+      pkgs.git
+    ];
   }
