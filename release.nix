a2e0f1b34dbbb4c29adcb6f58198eaa2a2187c22
Ali Abrar <ali.abrar@obsidian.systems>
2023-08-21 11:01:53 -0400

    ci: Cache multiple jdks

--- release.nix
+++ release.nix
@@ -1,18 +1,23 @@
 { project ? import ./.
 , supportedSystems ? ["x86_64-linux" "x86_64-darwin"]
+, jdks ? ["jdk" "jdk17" "jdk19"]
 }:
 let
 
   native-pkgs = (project {}).pkgs;
   inherit (native-pkgs) lib;
 
-  perPlatform = lib.genAttrs supportedSystems (system: let
-    args = {
-      inherit system;
-    };
-  in lib.mapAttrs (_: lib.recurseIntoAttrs) {
-    project = project args;
-    project-with-vim = project (args // { vimMode = true; });
-  });
+  allVersions = lib.genAttrs supportedSystems (system: let
 
-in lib.mapAttrs (_: lib.recurseIntoAttrs) perPlatform
+    perJdk = lib.genAttrs jdks (jdkVersion: let
+      args = {
+        inherit system jdkVersion;
+      };
+    in lib.mapAttrs (_: lib.recurseIntoAttrs) {
+      project = project args;
+      project-with-vim = project (args // { vimMode = true; });
+    });
+
+    in lib.mapAttrs (_: lib.recurseIntoAttrs) perJdk
+  );
+in lib.mapAttrs (_: lib.recurseIntoAttrs) allVersions
