26a98c81c4d17d4c03d090ae018ee27004f0698e
Sky O <soraskymizu@gmail.com>
2024-04-11 15:22:45 +0000

    Update PQS

--- default.nix
+++ default.nix
@@ -2,7 +2,7 @@
 , system ? builtins.currentSystem
 , jdkVersion ? "jdk"
 , sdkVersion ? "2.6.4"
-, scribeVersion ? "0.1.0"
+, scribeVersion ? "0.1.1"
 , sdkSpec ? builtins.fromJSON(builtins.readFile (./versions + "/${sdkVersion}.json"))
 , cantonEnterprise ? false
 , enableScribe ? false
---
+++ scribe-versions/0.1.1.nix
@@ -0,0 +1,3 @@
+{
+  sha256 = "02zy1qf3453zjwni97ypdrai6ga87b5zq1l17609xfjsavdac7nz";
+}
