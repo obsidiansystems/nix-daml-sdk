2bd4897a64d83f9bd26e39fa2f7fb7ebef874177
cidkidnix <cidkidnix@protonmail.com>
2025-08-05 08:17:02 -0500

    update sdk ver

--- shell.nix
+++ shell.nix
@@ -2,7 +2,7 @@
 , extraPackages ? (_:[])
 , system ? builtins.currentSystem
 , jdkVersion ? "jdk"
-, sdkVersion ? "2.6.4"
+, sdkVersion ? "3.3.0-snapshot.20250507.0"
 , sdkSpec ? builtins.fromJSON(builtins.readFile (./versions + "/${sdkVersion}.json"))
 , cantonEnterprise ? false
 }:
