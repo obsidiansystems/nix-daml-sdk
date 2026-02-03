b33282d0efad8ec0dd7bda74abe621ae5e236fdd
Riuga <riuga@tuta.io>
2026-02-03 12:40:26 -0600

    Bump daml version to 3.4.10

--- default.nix
+++ default.nix
@@ -1,7 +1,7 @@
 { vimMode ? false , extraPackages ? (_:[])
 , system ? builtins.currentSystem
 , jdkVersion ? "jdk"
-, sdkVersion ? "2.6.4"
+, sdkVersion ? "3.4.10"
 , scribeVersion ? "0.1.0"
 , sdkSpec ? builtins.fromJSON(builtins.readFile (./versions + "/${sdkVersion}.json"))
 , cantonEnterprise ? false
